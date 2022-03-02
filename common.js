83af9f14892d3eb0a75e1ec630201dc48e2834d3
Rattanapong <2010511101032@live4.utcc.ac.th>
2022-03-02 14:38:11 +0700

    refactoring#1 add validate fn

--- common.js
+++ common.js
@@ -1,16 +1,21 @@
+function validate(p){
+	if(isNaN(pace)){
+		msg = "Please verify that the pace you have entered is correct!!!";
+		return {"status":0,"message":msg,"hour":0,"min":0};
+	}
+	if(!(pace >= 3 && pace <= 10)){
+		msg = "Running pace must be between 3 and 10 min/km";
+		return {"status":0,"message":msg,"hour":0,"min":0};
+	}
+}
+		
 function calculate(p,d){
 		var msg = "";
 		var pace = parseFloat(p);
 		var dist = parseInt(d);
 	//---------------------------
-		if(isNaN(pace)){
-			msg = "Please verify that the pace you have entered is correct!!!";
-			return {"status":0,"message":msg,"hour":0,"min":0};
-		}
-		if(!(pace >= 3 && pace <= 10)){
-			msg = "Running pace must be between 3 and 10 min/km";
-			return {"status":0,"message":msg,"hour":0,"min":0};
-		}
+	    var chk = validate(pace);
+		if(chk !=null)return chk;
 	//---------------------------
 		var min = dist * pace;
 		var hour = 0;
